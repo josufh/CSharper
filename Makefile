4a79a30826b55aa014c965281dcac8d1bfdce867
Joshua <ferrer-joshua@nk-create.co.jp>
2025-11-20 17:15:02 +0900

    GC_VERBOSE macro introduced for testing

--- Makefile
+++ Makefile
@@ -9,6 +9,9 @@ SRC_OBJS := $(SRC_SRCS:.c=.o)
 TEST_SRCS := $(wildcard tests/*.c)
 TEST_BINS := $(patsubst tests/%.c,tests/%,$(TEST_SRCS))
 
+TEST_CFLAGS := $(CFLAGS) -DGC_VERBOSE
+TEST_OBJS   := $(SRC_SRCS:.c=.test.o)
+
 .PHONY: all build test clean
 
 all: build
@@ -29,11 +32,14 @@ test: $(TEST_BINS)
 	done
 	@rm -f $(TEST_BINS)
 
-tests/%: tests/%.c $(SRC_OBJS)
-	$(CC) $(CFLAGS) $^ -o $@
+tests/%: tests/%.c $(TEST_OBJS)
+	$(CC) $(TEST_CFLAGS) $^ -o $@
 
 src/%.o: src/%.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
+src/%.test.o: src/%.c
+	$(CC) $(TEST_CFLAGS) -c $< -o $@
+
 clean:
-	rm -f $(SRC_OBJS) $(TEST_BINS) $(PROGRAM) main.o
+	rm -f $(SRC_OBJS) $(TEST_OBJS) $(TEST_BINS) $(PROGRAM) main.o
--- src/gc.c
+++ src/gc.c
@@ -2,11 +2,21 @@
 
 #include <stdlib.h>
 
+#ifdef GC_VERBOSE
+#include <stdio.h>
+#endif
+
 static void **gc_ptrs = NULL;
 static size_t gc_count = 0;
 static size_t gc_capacity = 0;
 
-static void GC_Register(void *ptr) {
+#ifdef GC_VERBOSE
+static size_t  *gc_sizes       = NULL;
+static size_t   gc_total_bytes = 0;
+static size_t   gc_max_alloc   = 0;
+#endif
+
+static void GC_Register(void *ptr, size_t size) {
     if (!ptr) return;
     if (gc_count == gc_capacity) {
         size_t new_cap = gc_capacity == 0 ? 16 : gc_capacity * 2;
@@ -16,9 +26,29 @@ static void GC_Register(void *ptr) {
             abort();
         }
         gc_ptrs = new_buf;
+
+#ifdef GC_VERBOSE
+        size_t *new_sizes = realloc(gc_sizes, new_cap * sizeof(size_t));
+        if (!new_sizes) {
+            free(ptr);
+            abort();
+        }
+        gc_sizes = new_sizes;
+#endif
+
         gc_capacity = new_cap;
     }
-    gc_ptrs[gc_count++] = ptr;
+    gc_ptrs[gc_count] = ptr;
+
+#ifdef GC_VERBOSE
+    gc_sizes[gc_count] = size;
+    gc_total_bytes += size;
+    if (size > gc_max_alloc)
+        gc_max_alloc = size;
+#endif
+
+    gc_count++;
+    (void)size;
 }
 
 void *GC_Alloc(size_t size) {
@@ -26,7 +56,7 @@ void *GC_Alloc(size_t size) {
     if (!ptr) {
         exit(69);
     }
-    GC_Register(ptr);
+    GC_Register(ptr, size);
     return ptr;
 }
 
@@ -34,6 +64,21 @@ static void GC_FreeAll(void) {
     for (size_t i = 0; i < gc_count; i++) {
         free(gc_ptrs[i]);
     }
+
+#ifdef GC_VERBOSE
+    fprintf(stderr, "[GC] allocations: %zu\n", gc_count);
+    fprintf(stderr, "[GC] total bytes: %zu\n", gc_total_bytes);
+    fprintf(stderr, "[GC] max single allocation: %zu\n", gc_max_alloc);
+    if (gc_count > 0) {
+        fprintf(stderr, "[GC] average allocation: %.2f\n",
+                (double)gc_total_bytes / (double)gc_count);
+    }
+    free(gc_sizes);
+    gc_sizes = NULL;
+    gc_total_bytes = 0;
+    gc_max_alloc   = 0;
+#endif
+
     free(gc_ptrs);
     gc_ptrs = NULL;
     gc_count = 0;

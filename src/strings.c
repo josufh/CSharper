a3bacfeff674e012f4b5d9b57867cdaa55e960a9
Joshua <ferrer-joshua@nk-create.co.jp>
2025-11-20 17:45:07 +0900

    change the String struct to have ByteCount and Length separate

--- src/strings.c
+++ src/strings.c
@@ -4,14 +4,20 @@
 #include "../include/gc.h"
 
 static String Strings_NewFn(const char* text) {
-    size_t len = strlen(text);
+    size_t byte_len  = strlen(text);
     
-    char* buf = (char*)GC_Alloc(len + 1);
-    if (buf) {
-        memcpy(buf, text, len + 1);
+    char* buf = (char*)GC_Alloc(byte_len  + 1);
+    memcpy(buf, text, byte_len  + 1);
+
+    size_t char_len = 0;
+    for (size_t i = 0; i < byte_len; ++i) {
+        unsigned char c = (unsigned char)buf[i];
+        if ((c & 0xC0) != 0x80) {
+            ++char_len;
+        }
     }
     
-    String string = {buf, len};
+    String string = {buf, char_len};
     return string;
 }
 

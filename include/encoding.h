c2a5d9a656e4dfa732b92219c61aee3430721026
Joshua Ferrer <josuferrer@outlook.com>
2025-11-20 22:27:05 +0900

    Encoding_GetString

--- include/encoding.h
+++ include/encoding.h
@@ -6,9 +6,11 @@
 
 typedef struct EncodingStruct EncodingStruct;
 typedef Bytes (*GetBytesFn)(String);
+typedef String (*GetStringFn)(Bytes);
 
 struct EncodingStruct {
     GetBytesFn GetBytes;
+    GetStringFn GetString;
 };
 
 extern const EncodingStruct Encoding;
--- src/encoding.c
+++ src/encoding.c
@@ -5,13 +5,34 @@
 #include "gc.h"
 
 static Bytes Encoding_GetBytes(String string) {
-    unsigned char* buf = (unsigned char*)GC_Alloc(string.Length);
-    memcpy(buf, string.Value, string.Length);
+    size_t len = strlen(string.Value);
+    unsigned char* buf = (unsigned char*)GC_Alloc(len);
+    memcpy(buf, string.Value, len);
     
-    Bytes bytes = {buf, string.Length};
+    Bytes bytes = {buf, len};
     return bytes;
 }
 
+static String Encoding_GetString(Bytes bytes) {
+    size_t byte_len = bytes.Length;
+
+    char* buf = (char*)GC_Alloc(byte_len + 1);
+    memcpy(buf, bytes.Data, byte_len);
+    buf[byte_len] = '\0';
+
+    size_t char_len = 0;
+    for (size_t i = 0; i < byte_len; ++i) {
+        unsigned char c = (unsigned char)buf[i];
+        if ((c & 0xC0) != 0x80) {
+            ++char_len;
+        }
+    }
+
+    String string = { buf, char_len };
+    return string;
+}
+
 const EncodingStruct Encoding = {
-    .GetBytes = Encoding_GetBytes
+    .GetBytes = Encoding_GetBytes,
+    .GetString = Encoding_GetString
 };
